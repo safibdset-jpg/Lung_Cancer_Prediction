f0d74762e17c1f7a715eda22e6b951080fc136e8
safibdset-jpg <safi.bdset@gmail.com>
2026-02-08 16:10:32 +0600

    first commit

---
+++ frontend/script.js
@@ -0,0 +1,35 @@
+function predict() {
+    const data = {
+        GENDER: gender.value, // This is encoded in your notebook (M=1, F=0)
+        AGE: Number(age.value),
+        SMOKING: Number(smoking.value),
+        YELLOW_FINGERS: Number(yellow_fingers.value),
+        ANXIETY: Number(anxiety.value),
+        PEER_PRESSURE: Number(peer_pressure.value),
+        CHRONIC_DISEASE: Number(chronic_disease.value),
+        FATIGUE: Number(fatigue.value),
+        ALLERGY: Number(allergy.value),
+        WHEEZING: Number(wheezing.value),
+        ALCOHOL_CONSUMING: Number(alcohol_consuming.value),
+        COUGHING: Number(coughing.value),
+        SHORTNESS_OF_BREATH: Number(shortness_of_breath.value),
+        SWALLOWING_DIFFICULTY: Number(swallowing_difficulty.value),
+        CHEST_PAIN: Number(chest_pain.value)
+};
+
+    fetch("http://127.0.0.1:8000/predict", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(result => {
+        document.getElementById("result").innerText = result.result;
+    })
+    .catch(err => {
+        alert("API Error");
+        console.error(err);
+    });
+}
---
+++ main.py
@@ -0,0 +1,87 @@
+# uvicorn main:app --reload
+
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field, field_validator
+import joblib
+import numpy as np
+import os
+
+# ==============================
+# Load ML model (safe path)
+# ==============================
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "backend/model.pkl")
+
+if not os.path.exists(MODEL_PATH):
+    raise RuntimeError("❌backend/scaler.pkl  file not found")
+
+model = joblib.load(MODEL_PATH)
+
+# ==============================
+# FastAPI app
+# ==============================
+app = FastAPI(
+    title="Laung Cancer Prediction API",
+    version="1.0"
+)
+
+# ==============================
+# CORS (Frontend connect)
+# ==============================
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # production এ specific domain দিবে
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ==============================
+# Input Schema with STRICT validation
+# ==============================
+class LungCancerInput(BaseModel):
+    GENDER: int = Field(..., gt=0, le=1)
+    AGE: int = Field(..., ge=0, description="Age must be > 0")
+    SMOKING: int = Field(..., ge=1, le=2)
+    YELLOW_FINGERS: int = Field(..., gt=1, le=2)
+    ANXIETY: int = Field(..., ge=1, le=2)
+    PEER_PRESSURE: int = Field(..., ge=1, le=2)
+    CHRONIC_DISEASE: int = Field(..., ge=1, le=2)
+    FATIGUE: int = Field(..., gt=1, le=2)
+    ALLERGY: int = Field(..., ge=1, le=2)
+    WHEEZING: int = Field(..., ge=1, le=2)
+    ALCOHOL_CONSUMING: int = Field(..., ge=1, le=2)
+    COUGHING: int = Field(..., ge=1, le=2)
+    SHORTNESS_OF_BREATH: int = Field(..., ge=1, le=2)
+    SWALLOWING_DIFFICULTY: int = Field(..., ge=1, le=2)
+    CHEST_PAIN: int = Field(..., ge=1, le=2)
+    # Blank / empty check
+    @field_validator("*", mode="before")
+    @classmethod
+    def no_blank_value(cls, v):
+        if v is None or v == "":
+            raise ValueError("Field cannot be blank")
+        return v
+
+# ==============================
+# Routes
+# ==============================
+@app.get("/")
+def health():
+    return {"status": "API running successfully"}
+
+@app.post("/predict")
+def predict(data: LungCancerInput):
+
+    values = list(data.model_dump().values())
+
+
+    features = np.array([values])
+
+    prediction = model.predict(features)[0]
+
+    return {
+        "prediction": int(prediction),
+        "result": "Lung Cancer Detected" if prediction == 1 else "No LAung Cancer"
+    }
